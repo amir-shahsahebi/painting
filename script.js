0c14201be2cdf6d0ebf7fed4a7cc7ed9aa79ad67
amir shahsahebi <shahsahebiamir@gmail.com>
2022-02-08 16:43:53 +0330

    adding pen size

--- script.js
+++ script.js
@@ -28,19 +28,41 @@ palet.addEventListener("click", (e)=>{
         }
         color = e.target.style.backgroundColor
         e.target.style.borderRadius = "50px"
+        eraser=false
+        
+        btnEraser.style.backgroundColor = "white";
+        btnEraser.style.color = "black";
     }
-    console.dir(color)
+    // console.dir(color)
 })
 makeColor()
-// let board = document.querySelector(".white-board")
-// board.addEventListener("mousemove",(e)=>{
-//     dot = document.createElement("div");
-//     dot.classList.add("p1");
-//     dot.style.backgroundColor= color;
-//     dot.style.top= e.clientY + "px";
-//     dot.style.left= e.clientX + "px";
-//     board.append(dot)
-// })
+//define pen
+let penSize = "5px";
+pen1 = document.querySelector(".pen1")
+pen2 = document.querySelector(".pen2")
+pen3 = document.querySelector(".pen3")
+pen1.style.boxShadow = "5px 3px 3px rgba(0, 0, 0, 0.486)";
+
+pen1.addEventListener("click",()=>{
+     penSize = "5px"
+    pen1.style.boxShadow = "5px 3px 3px rgba(0, 0, 0, 0.486)";
+    pen2.style.boxShadow = "0px 0px 0px rgba(0, 0, 0, 0.486)";
+    pen3.style.boxShadow = "0px 0px 0px rgba(0, 0, 0, 0.486)";
+})
+pen2.addEventListener("click",()=>{
+     penSize = "10px"
+    pen2.style.boxShadow = "5px 3px 3px rgba(0, 0, 0, 0.486)";
+    pen1.style.boxShadow = "0px 0px 0px rgba(0, 0, 0, 0.486)";
+    pen3.style.boxShadow = "0px 0px 0px rgba(0, 0, 0, 0.486)";
+})
+pen3.addEventListener("click",()=>{
+     penSize = "15px"
+    pen3.style.boxShadow = "5px 3px 3px rgba(0, 0, 0, 0.486)";
+    pen1.style.boxShadow = "0px 0px 0px rgba(0, 0, 0, 0.486)";
+    pen2.style.boxShadow = "0px 0px 0px rgba(0, 0, 0, 0.486)";
+})
+
+
 let mouseIsDown = false
 board.addEventListener('mousedown', function(){
     mouseIsDown = true
@@ -48,14 +70,16 @@ board.addEventListener('mousedown', function(){
 board.addEventListener('mouseup', function(){
     mouseIsDown = false
 })
-
+let eraser = false
 board.addEventListener("mousemove",(e)=>{
-    if (mouseIsDown) {
+    if (mouseIsDown && !eraser) {
     dot = document.createElement("div");
     dot.classList.add("p1");
     dot.style.backgroundColor= color;
     dot.style.top= e.clientY + "px";
     dot.style.left= e.clientX + "px";
+    dot.style.width = penSize;
+    dot.style.height = penSize;
     board.append(dot)
     }
 })
@@ -66,7 +90,8 @@ resetColor.addEventListener("click" ,  ()=> {
     for (co of colors) {
         co.remove()
     }
-    console.log(colors)
+    // let btnEraser = document.querySelector(".eraser")
+    // console.log(colors)
     makeColor()
 })
 
@@ -86,5 +111,35 @@ backgroundBoard.addEventListener("click",()=> {
 }) 
 
 //eraser 
-let btnEraser = document.createElement("button")
-btnEraser.classList.add(".reset-board")
+    let btnEraser = document.createElement("button");
+    btnEraser.classList.add(".eraser");
+    btnEraser.style.position = "absolute";
+    btnEraser.style.bottom= "-15px"
+    btnEraser.style.left= "7px"
+    btnEraser.style.width= "85%"
+    btnEraser.style.height= "25px"
+
+    btnEraser.innerText = "Eraser";
+    palet.append(btnEraser);
+    btnEraser.addEventListener("click",(t)=> {
+        eraser = true
+            btnEraser.style.backgroundColor = "red";
+            btnEraser.style.color = "white";
+        // t.stopImmediatePropagation();
+        let mouseIsDown = false;
+        board.addEventListener("mousedown", function () {
+            mouseIsDown = true;
+        });
+        board.addEventListener("mouseup", function () {
+            mouseIsDown = false;
+        });
+        
+        board.addEventListener("mousemove", (e) => {
+            if (mouseIsDown) {
+                if (e.target.nodeName==="DIV" && e.target.className ==="p1") {
+                    e.target.remove()
+                }
+            }
+        });
+        
+    })
